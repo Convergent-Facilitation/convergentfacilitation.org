55426a9f3b211d039aa997629dcbf39eba590f4e
f <f@email>
2024-01-03 21:30:11 +0100

    updated aimees quote and added names to quotes

--- src/data/quotes.tsx
+++ src/data/quotes.tsx
@@ -13,33 +13,25 @@ import Translate, {translate} from '@docusaurus/Translate';
 const QUOTES = [
   {
     thumbnail: require('./quotes/tim-mahoney.png'),
-    name: '',
-    title: translate({
-      id: 'homepage.quotes.christopher-chedeau.title',
-      message: '',
-      description: 'Title of quote of Christopher Chedeau on the home page',
-    }),
+    name: 'Tim Mahoney',
+	title: '',
     text: 'I really had no interest nor any belief that this process would actually do anything. However, the trust that it built was quite amazing to me. I wouldn’t have believed it was possible, but we achieved more collaboratively than we were able to do as adversaries.” There was a moment when it became apparent to me that people had become problem-solvers rather than position-staters.'
   },
   {
     thumbnail: require('./quotes/aimee_ryan.png'),
-    name: '',
-     title: translate({
-      id: 'homepage.quotes.hector-ramos.title',
-      message: '',
-      description: '',
-    }),
-    text: 'At some point, we hit a really tough ‘content-based’ challenge. It became clear that it was ‘THE’ issue. If we didn’t crack it, we wouldn’t reach consensus on the overall project. The field didn’t need another paper on so and so’s opinion about various solutions. We were here to figure out a frame that held them all, and we eventually succeeded. '
+    name: 'Aimee Ryan',
+    title: '',
+    text: "At some point, we came upon the issue that was THE issue; the one that, if we didn't crack, we wouldn't reach consensus on the overall project. When we didn't think we could find consensus, we created the opportunity to simply publish the opposing viewpoints. Eventually, we agreed that the field didn’t need another report that outlined the polarizing perspectives. So instead, we dug deeper, expanded our creativity, and developed a frame, and solutions, that held us all."
   },
   {
     thumbnail: require('./quotes/vanda.png'),
-    name: '',
+    name: 'Vanda Perez Bessone',
     title: '',
     text: 'As soon as we started meeting, we had fun. Parents, staff and board members didn’t experience the decision process as a debate - no one tried to convince anyone else - and our discussions didn’t feel endless. The main shift happened when we discovered we could integrate two pathways we initially thought of as opposite. We could merge them into a parallel and complementary model that works beautifully.'
   },
   {
     thumbnail: require('./quotes/katy-mamen.png'),
-    name: '',
+    name: 'Katy Mamen',
     title: '',
     text: 'I came up against a lot of intense power struggles when I was an activist. Instead, what I witnessed in this process was completely different: people representing different parts of the system accepted to come and stayed together across real differences, went to the heart of thorny issues, and came out at the other end with realistic, durable and robust strategies.'
   },
