926c22a63351971cb5cf8b7309cf288b7caf161c
f <f@email>
2024-01-10 09:35:41 +0100

    updated formatting and text of localtime display

--- src/components/LocalTime.jsx
+++ src/components/LocalTime.jsx
@@ -21,7 +21,7 @@ class LocalTimeFromUTC extends React.Component {
 			const isoDateString = `${this.props.year}${String(this.props.month).padStart(2,'0')}${String(this.props.day).padStart(2,'0')}T${String(this.props.startHour).padStart(2,'0')}${String(this.props.startMinute).padStart(2,'0')}00`;
 
 			return (
-				<span> <b>{startTime.toLocaleDateString('en-US', {weekday: 'long',  month: 'short',day: 'numeric', year: 'numeric' })} from  {startTime.toLocaleTimeString(userLocale, { hour: '2-digit', minute:'2-digit', })} to  {endTime.toLocaleTimeString(userLocale, { hour: '2-digit', minute:'2-digit', })}</b>, {timeZone} time (<a href={`https://www.timeanddate.com/worldclock/converter.html?iso=${isoDateString}&p1=tz_hast&p2=224&p3=179&p4=133&p5=37&p6=176&p7=tz_hkt&p8=240`}>click for more time zones</a>)</span>
+				<span> <b>{startTime.toLocaleDateString('en-US', {weekday: 'long',  month: 'short',day: 'numeric', year: 'numeric' })}</b> from  <b>{startTime.toLocaleTimeString(userLocale, { hour: '2-digit', minute:'2-digit', })} to  {endTime.toLocaleTimeString(userLocale, { hour: '2-digit', minute:'2-digit', })}</b>, {timeZone} time (<a href={`https://www.timeanddate.com/worldclock/converter.html?iso=${isoDateString}&p1=tz_hast&p2=224&p3=179&p4=133&p5=37&p6=176&p7=tz_hkt&p8=240`}>more time zones</a>)</span>
 			);
 		}}
       </BrowserOnly>
